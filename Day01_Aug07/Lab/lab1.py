a37f1b4cbf34b7a9992fa4c14c04c4c7a123a167
nw-waterbury <nw-waterbury@users.noreply.github.com>
2017-08-07 15:08:11 -0500

    classlab1

--- Day01_Aug07/Lab/lab1.py
+++ Day01_Aug07/Lab/lab1.py
@@ -1,48 +1,70 @@
-def binarify(num): 
+def binarify(num):
   """convert positive integer to base 2"""
   if num<=0: return '0'
-  digits = []
+  else:
+      digits = []
+      while num>0:
+          new_numb=num/2
+          remain=str(num%2)
+          digits.insert(0,remain)
+          num=new_numb
   return ''.join(digits)
 
 def int_to_base(num, base):
   """convert positive integer to a string in any base"""
-  if num<=0:  return '0' 
-  digits = []
+  if num<=0: return '0'
+  else:
+      digits = []
+      while num>0:
+          new_numb=num/base
+          remain=str(num%base)
+          digits.insert(0,remain)
+          num=new_numb
   return ''.join(digits)
 
 def base_to_int(string, base):
-  """take a string-formatted number and its base and return the base-10 integer"""
-  if string=="0" or base <= 0 : return 0 
-  result = 0 
-  return result 
+"""take a string-formatted number and its base and return the base-10 integer"""
+    result=int(string,base)
+    return result
+
 
 def flexibase_add(str1, str2, base1, base2):
   """add two numbers of different bases and return the sum"""
-  result = int_to_base(tmp, base1)
-  return result 
+  num_one=base_to_int(str1, base1)
+  num_two=base_to_int(str2, base2)
+  result=num_one+num_two
+  return result
 
 def flexibase_multiply(str1, str2, base1, base2):
   """multiply two numbers of different bases and return the product"""
-  result = int_to_base(tmp, base1)
-  return result 
+  num_one=base_to_int(str1, base1)
+  num_two=base_to_int(str2, base2)
+  result = num_one*num_two
+  return result
 
 def romanify(num):
   """given an integer, return the Roman numeral version"""
-  result = ""
-  return result
-  
+   ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+   romans = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+   result = ""
+   for i in range(len(ints)):
+      count = int(num / ints[i])
+      result += romans[i] * count
+      num -= ints[i] * count
+   return result
+
 # Copyright (c) 2014 Matt Dickenson
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
