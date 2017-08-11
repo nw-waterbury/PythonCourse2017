20d5ce7144ef69e56d8c212cc62179ea20744179
nw-waterbury <nw-waterbury@users.noreply.github.com>
2017-08-11 11:50:54 -0500

    amended4

--- Day03_Aug11/Exercises/ordinal_nw.py
+++ Day03_Aug11/Exercises/ordinal_nw.py
@@ -1,9 +1,9 @@
 def ordinal(numb):
         if type(numb)!= int:
-
+            return "Not a valid input!"
         else:
             numb=str(numb)
             end = {'1':'st', '2':'nd', '3':'rd'}
             if numb[-2:-1] == '1': return numb+'th'
-            elif numb[-1] in endings.keys(): return numb+ endings[numb[-1]]
+            elif numb[-1] in end.keys(): return numb+ end[numb[-1]]
             else: return numb+'th'
