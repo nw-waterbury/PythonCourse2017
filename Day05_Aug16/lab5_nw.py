0f395c8fa19add9bf79a6118d79449a5ff15a9d7
nw-waterbury <nw-waterbury@users.noreply.github.com>
2017-08-16 14:44:10 -0500

    Lab 5 finished

---
+++ Day05_Aug16/lab5_nw.py
@@ -0,0 +1,79 @@
+# pip install googlemaps
+from googlemaps import Client
+from datetime import datetime
+
+
+api_key = 'yourkey'
+gmaps = Client('a key')
+dir(gmaps)
+whitehouse = '1600 Pennsylvania Avenue, Washington, DC'
+whitehouse_geoloc = gmaps.geocode(whitehouse)
+print whitehouse_geoloc
+
+destination = gmaps.reverse_geocode((38.897096, -77.036545))
+print destination
+
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+lat_long = (gmaps.geocode('326 Perkins Library, Durham, NC 27708')[0]['geometry']['location']['lat'], gmaps.geocode('326 Perkins Library, Durham, NC 27708')[0]['geometry']['location']['lng'])
+print lat_long
+duke = gmaps.reverse_geocode(lat_long)[0]['formatted_address']
+print duke
+
+local = gmaps.places('restaurant near ' + duke)
+print local['results'][0]['formatted_address']
+print local['results'][0]['name']
+
+directions = gmaps.directions(duke, whitehouse)
+print directions[0]['legs'][0]['distance']
+
+for step in directions[0]['legs'][0]['steps']:
+	print step['html_instructions']
+
+def nearest_embassy(wh, embassy_list):
+    e_dist = distances(wh, embassy_list)
+    shortest = min(e_dist)
+    identify = e_dist.index(shortest)
+    print "This is the closest location: %s."  %(embassy_list[identify])
+    adres=address(embassy_list[identify])
+    print "It is located at this address: %s." % (adres)
+    return identify
+
+
+def distances(wh, embassy_list):
+	wh_emb_dist = []
+	if type(wh) != str:
+		wh = gmaps.reverse_geocode(wh)[0]['geometry']['location']
+	for place in embassy_list:
+		latlong = gmaps.reverse_geocode(place)[0]['geometry']['location']
+		dist = gmaps.distance_matrix(wh, latlong)
+		wh_emb_dist.append(dist['rows'][0]['elements'][0]['distance']['value'])
+	return wh_emb_dist
+
+def address(embassy):
+	return gmaps.reverse_geocode(embassy)[0]['formatted_address']
+# if I wanted to hold a morning meeting there, which cafe would you suggest?
+def nearest_venue(wh, embassy_list, venue):
+    near = nearest_embassy(wh, embassy_list)
+    embassy = wh[near]
+    search = gmaps.places(query = venue, location = embassy)['results']
+    possibilities = []
+    for hit in search:
+        location = hit['geometry']['location']
+        latlong = [location['lat'], location['lng']]
+        possibilities.append(latlong)
+    near_venue = nearest_embassy(embassy, possibilities)
+    venue_name=str(search[near_venue]['name'])
+    #venue_add=str(search[near_venue]['formatted_address'])
+    return venue_name
+
+find_place(embassies)
+
+nearest_venue(whitehouse, embassies, 'cafe in DC')
+# if I wanted to hold an evening meeting there, which bar would you suggest?
+
+nearest_venue(whitehouse, embassies, 'bars in DC')
