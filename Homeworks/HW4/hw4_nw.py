668b0554299769c27817cea95605b6c0ce92a2d3
nw-waterbury <nw-waterbury@users.noreply.github.com>
2017-08-24 09:13:47 -0500

    fixing some simple errors

--- Homeworks/HW4/hw4_nw.py
+++ Homeworks/HW4/hw4_nw.py
@@ -1,13 +1,14 @@
 
 def insertionSort(list_numbers):
     """Sorting Algorithm 1: Insertion Sort - O(n^2)"""
-	n = len(list_numbers)
-	for position in range(1,n):
-		current = list_numbers[position]
-		while position >= 1 and list_numbers[position-1]>current:
-			list_numbers[position]=list_numbers[position-1]
-			position+=-1
-		list_numbers[position] = current
+    n = len(list_numbers)
+    for position in range(1,n):
+        current = list_numbers[position]
+        while position >= 1 and list_numbers[position-1]>current:
+            list_numbers[position]=list_numbers[position-1]
+            position+=-1
+        list_numbers[position] = current
+    return list_numbers
 
 def bubbleSort(list_numbers):
     for j in range(0,len(list_numbers)):
