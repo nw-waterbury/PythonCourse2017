8ba5e320808a62220b5e31d13e6d32a8550f5933
nw-waterbury <nw-waterbury@users.noreply.github.com>
2017-08-24 09:08:00 -0500

    made variable names consistent

--- Homeworks/HW4/hw4_nw.py
+++ Homeworks/HW4/hw4_nw.py
@@ -1,19 +1,19 @@
 
-def insertionSort(numbers):
-    """Sorting Algorithm 2: Insertion Sort - O(n^2)"""
-	n = len(numbers)
+def insertionSort(list_numbers):
+    """Sorting Algorithm 1: Insertion Sort - O(n^2)"""
+	n = len(list_numbers)
 	for position in range(1,n):
-		current = numbers[position]
-		while position >= 1 and numbers[position-1]>current:
-			numbers[position]=numbers[position-1]
+		current = list_numbers[position]
+		while position >= 1 and list_numbers[position-1]>current:
+			list_numbers[position]=list_numbers[position-1]
 			position+=-1
-		numbers[position] = current
-        
-def BubbleSort(list):
-    for j in range(0,len(list)):
-        for i in range(j+1, len(list)):
-            if list[i] >= list[j]:
+		list_numbers[position] = current
+
+def bubbleSort(list_numbers):
+    for j in range(0,len(list_numbers)):
+        for i in range(j+1, len(list_numbers)):
+            if list_numbers[i] >= list_numbers[j]:
                 pass
             else:
-                list[i], list[j] = list[j], list[i]
-    return list
+                list_numbers[i], list_numbers[j] = list_numbers[j], list_numbers[i]
+    return list_numbers
